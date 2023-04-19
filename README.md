cff27e1c8b9b700ed7298c960bbd1cd9b4d03c8a
Keara Berlin <kearaberlin@gmail.com>
2023-04-18 22:57:53 -0500

    automatically scrape the correct number of pages for all biomedical related encyclopedias

---
+++ README.md
@@ -0,0 +1,25 @@
+# NLP Final Project
+
+Spring 2023
+
+## Required Packages
+
+pip install:
+
+beautifulsoup4
+
+pandas
+
+numpy
+
+nltk 
+
+jieba
+
+## main.py
+
+Run this file to scrape online Chinese-English biomedical term dictionary.
+
+## dataset_import.py 
+
+Load ParaMed parallel corpus for English-Chinese sentence level translation in the biomedical domain.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import re
 
 def get_page_html(url):
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
@@ -20,16 +21,40 @@ def find_word_list(page_html):
             writer.writerow([tuple[0], tuple[1]])
     return 0
 
+def get_n_pages(url):
+    page_html = get_page_html(url)
+    soup = BeautifulSoup(page_html, 'html.parser')
+    # the text we want is nested in a structure: <div class="seasonnav"><h5> text <h5/></div>
+    nav = soup.find('div', {'class': 'seasonnav'})
+    # text should be in the form "<n> pages with <x> words"
+    text = nav.h5.text
+    numbers = re.findall(r'\d+', text)
+    return int(numbers[0])
 
-url = "https://dict.bioon.com/elite.asp?classid=7&page="
+
+url = "https://dict.bioon.com/elite.asp?classid="
+classids = [1,2,4,5,6,7,8,9,
+            10,11,13,14,15,16,17,18,
+            20,21,23,24,25,26,27, 
+            32,36,60,61,                
+            110,111,112,113,114,115,116,
+            121,133]
+url_page_suffix = "&page="
+
 with open('medical_translations.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['English', 'Chinese'])
-for i in range(5):
-    num = str(i+1)
-    url_ = url + num
-    with open('medical_translations.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Page ' + num])
-    page_html = get_page_html(url_)
-    find_word_list(page_html)
+
+for classid in classids:
+    classid_str = str(classid)
+    url_no_page = url + classid_str + url_page_suffix
+    n_pages = get_n_pages(url_no_page)
+
+    for i in range(n_pages):
+        num = str(i+1)
+        url_ = url + classid_str + url_page_suffix + num
+        with open('medical_translations.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Page ' + num])
+        page_html = get_page_html(url_)
+        find_word_list(page_html)
