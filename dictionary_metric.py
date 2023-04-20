2aafbaec0b42c22cd4361e458289b4038c57f6d6
Keara Berlin <kearaberlin@gmail.com>
2023-04-19 23:35:18 -0500

    implement dictionary accuracy code - needs to be tested

--- dictionary_metric.py
+++ dictionary_metric.py
@@ -6,57 +6,69 @@ def calculate_dictionary_accuracy(sentence, dict):
     """Returns the dictionary accuracy of the given sentence.
     Inputs:
         sentence (str): the untokenized sentence
-        dict (dict): dictionary with keys being source language terms and values being target language translations
-    Output:
+        dict (dict): dictionary where each key is a source language term and each value is a list of the possible target language translations
         (float) the dictionary accuracy of the sentence
     """
-    pass 
+    correct_terms = []
+    terms = get_terms(sentence, dict)
+    for term in terms:
 
-def corpus_level_dictionary_accuracy(infos, dict):
-    """Weighted average of dictionary accuracies by # of dictionary terms in sentence
+        translations = dict[term]
+        if any_in(translations, sentence):
+            correct_terms.append(term)
+
+    acc = len(correct_terms) / len(terms)
+    return acc
+
+def corpus_level_dictionary_accuracy(sentences, dict):
+    """Weighted average of dictionary accuracies, weighted by # of dictionary terms in sentence.
     Inputs:
-        infos (list): a list of dictionaries of the form {'sentence':str, 'n_terms':int, 'acc':float}
-                        where n_terms is the number of dictionary terms in that sentence.
-                        sentence is optional if both n_terms and acc are given.
-                        acc and n_terms are optional if sentence is given.
-        dict (dict): dictionary with keys being source language terms and values being target language translations
-    Output:
-        (float) the weighted average dictionary accuracy of the dataset
+        sentences (list of strs): list of untokenized source language sentences in the corpus
+        dict (dict): dictionary where each key is a source language term and each value is a list of the possible target language translations
+    Output: 
+        (float) the weighted average dictionary accuracy of the corpus
     """
-    SENTENCE_KEY = 'sentence'
-    N_TERMS_KEY = 'n_terms'
-    ACC_KEY = 'acc'
-
     numerator = 0
     denominator = 0
+    for sentence in sentences:
 
-    for info in infos:
-        ks = info.keys()
-
-        if N_TERMS_KEY in ks and ACC_KEY in ks:
-            weight = info[N_TERMS_KEY]
-            acc = info[ACC_KEY]
-        
-        elif SENTENCE_KEY in ks:
-            sentence = info[SENTENCE_KEY]
-            weight = count_n_terms(sentence, dict)
-            acc = calculate_dictionary_accuracy(sentence, dict)
-
-        else:
-            acc = 0
-            weight = 1
+        weight = len(get_terms(sentence, dict))
+        acc = calculate_dictionary_accuracy(sentence, dict)
 
         numerator += acc * weight
         denominator += weight
 
     return numerator / denominator
 
-def count_n_terms(sentence, dict):
-    """Return the number of terms (keys) from dict that are in sentence. Ignore proper subterms.
+def get_terms(sentence, dict):
+    """Return the terms (keys) from dict that are in sentence. Ignore proper subterms.
     Inputs:
-        sentence (str): the untokenized sentence
-        dict (dict): dictionary with keys being source language terms and values being target language translations
+        sentence (str): the untokenized source language sentence
+        dict (dict): dictionary where each key is a source language term and each value is a list of the possible target language translations
     Output: 
-        (int) the number of terms from dict in sentence
+        (list of strs) the terms from dict that were in sentence
     """
-    pass
+    sentence_terms = []
+    for term in dict.keys():
+        if term in sentence and not is_subterm(term, sentence_terms):
+            sentence_terms.append(term)
+    return sentence_terms
+
+def is_subterm(term, terms):
+    """Returns whether term is a subterm of (can be found verbatim within) any term in terms.
+    For example, 'ovarian cancer' is a proper subterm of 'epithelial ovarian cancer', 
+    but not of 'ovarian carcinoma'.
+    is_subterm('ovarian cancer', ['teratoma', 'epithelial ovarian cancer', 'ovarian carcinoma']): True
+    is_subterm('ovarian cancer', ['teratoma', 'ovarian carcinoma']): False
+    """ 
+    for t in terms:
+        if term in t:
+            return True
+    return False
+
+def any_in(terms, sentence):
+    """Returns whether at least one term in terms can be found in sentence."""
+    for term in terms:
+        if term in sentence:
+            return True 
+    return False
