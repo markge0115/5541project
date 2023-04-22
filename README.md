f182a962e437ae93488e995c07e1f643bcc54f8a
Keara Berlin <kearaberlin@gmail.com>
2023-04-22 16:39:52 -0500

    function to import ParaMed files as a list of sentences

--- README.md
+++ README.md
@@ -6,20 +6,25 @@ Spring 2023
 
 pip install:
 
-beautifulsoup4
+- transformers
+- pandas
+- numpy
+- nltk 
+- jieba
+- sentencepiece
+- beautifulsoup4
+- requests
 
-pandas
+## Files
 
-numpy
-
-nltk 
-
-jieba
-
-## main.py
-
+### main.py
 Run this file to scrape online Chinese-English biomedical term dictionary.
 
-## dataset_import.py 
-
+### dataset_import.py 
 Load ParaMed parallel corpus for English-Chinese sentence level translation in the biomedical domain.
+
+### dictionary_metric.py 
+Provides methods to calculate dictionary accuracy at the sentence or document level.
+
+### t5_inference.py
+Uses T5 to translate ParaMed English sentences into Chinese.
--- dataset_import.py
+++ dataset_import.py
@@ -4,6 +4,9 @@ import nltk
 nltk.download('punkt')
 import jieba
 
+EN_FILENAMES = ['nejm.train.en', 'nejm.test.en', 'nejm.dev.en']
+ZH_FILENAMES = ['nejm.train.zh', 'nejm.test.zh', 'nejm.dev.zh']
+
 def read_into_str(filepath, encoding='utf-8'):
   str = ""
   with open(filepath, encoding=encoding) as f:
@@ -11,7 +14,32 @@ def read_into_str(filepath, encoding='utf-8'):
     str += ' '.join(lines)
   return str
 
-#importing the files as strings
+def read_into_list(filepath, encoding='utf-8', known_only=True):
+  """Read the given monolingual file into a list of strings (sentences).
+  Inputs: 
+    filepath (str): path to the file to read
+    encoding (str): encoding to use to read the file
+    known_only (bool): if true, only returns sentences with only known words (no @-@)
+  Output: (list of strs) list of sentences
+  """
+  with open(filepath, encoding=encoding) as f:
+    sentences = f.read().splitlines()
+  
+  UNK = '@-@'
+  if known_only:
+    sentences = [s for s in sentences if UNK not in s]
+
+  return sentences
+
+#importing the files as lists of sentences
+trainen_list = read_into_list('nejm.train.en')
+trainzh_list = read_into_list('nejm.train.zh')
+testen_list = read_into_list('nejm.test.en')
+testzh_list = read_into_list('nejm.test.zh')
+deven_list = read_into_list('nejm.dev.en')
+devzh_list = read_into_list('nejm.dev.zh')
+
+# importing the files as strings
 trainenstr = read_into_str('nejm.train.en')
 trainzhstr = read_into_str('nejm.train.zh')
 testenstr = read_into_str('nejm.test.en')
